96f838e63902a272b6527716dfa16539d550d450
senobumimunajat <48814577+senobumimunajat@users.noreply.github.com>
2020-06-02 10:58:22 +0700

    Details Page Finished

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import Fade from 'react-reveal/Fade';
 import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
+
 
 import ItemDetails from "json/itemDetails.json";
 
@@ -23,21 +27,29 @@ export default class DetailsPage extends Component {
 
         return (
             <>
-                <Header {...this.props}></Header>
+                <Header {...this.props} />
                 <PageDetailTitle
                     breadcrumb={breadcrumb}
                     data={ItemDetails}
-                ></PageDetailTitle>
-                <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+                />
+                <FeaturedImage data={ItemDetails.imageUrls} />
                 <section className="container">
                     <div className="row" style={{ marginTop: 50 }}>
                         <div className="col-7 pr-5">
-                            <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+                            <Fade bottom>
+                                <PageDetailDescription data={ItemDetails} />
+                            </Fade>
                         </div>
                         <div className="col-5">
-                            <BookingForm itemDetails={ItemDetails}></BookingForm></div>
+                            <Fade bottom>
+                                <BookingForm itemDetails={ItemDetails} />
+                            </Fade>
+                        </div>
                     </div>
                 </section>
+                <Categories data={ItemDetails.categories} />
+                <Testimony data={ItemDetails.testimonial} />
+                <Footer />
             </>
         );
     }
