6c0bcd927c71ad89ad2f81acd782a5a67320e39a
senobumimunajat <48814577+senobumimunajat@users.noreply.github.com>
2020-06-22 17:28:15 +0700

    Stepper Booking Page
    only script

---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+import propTypes from 'prop-types'
+
+export default function Stepper(props) {
+
+    const { steps, intialStep } = props;
+    const stepsKeys = Object.key(steps);
+
+    const [CurrentStep, setCurrentStep] = useState(
+        stepsKeys.indexOf(intialStep) > -1 ? intialStep : stepsKeys[0]
+    );
+    const totalStep = stepsKeys.length;
+    const indexStep = stepsKeys.indexOf(CurrentStep);
+
+    function prevStep() {
+        if (+indexStep > 0) setCurrentStep(stepsKeys[indexStep - 1]);
+    }
+
+    function nextStep() {
+        if (+indexStep < totalStep) setCurrentStep(stepsKeys[indexStep + 1]);
+    }
+
+    return <>{props.children(prevStep, nextStep, CurrentStep, steps)}</>;
+}
+
+Stepper.propTypes = {
+    data: propTypes.object.isRequired,
+    intialStep: propTypes.string.isRequired,
+};
